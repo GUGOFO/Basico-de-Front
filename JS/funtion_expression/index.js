51fa8253e2151fe1b62d9756ec78671c0706b51f
Gustavo <gugofogomes@gmail.com>
2026-02-13 15:15:02 -0300

    'correção de codigo'

--- JS/funtion_expression/index.js
+++ JS/funtion_expression/index.js
@@ -11,4 +11,3 @@ setTimeout(function(){
 }, 5000); //esse tempo é desde o começo da pagina, ent n continua dps do anterior 3 seg
 
 //isso oq fizemos foi basicamente passar uma função diretamente para a entrada, foda
-
