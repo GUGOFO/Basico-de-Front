0bde8903c10cb8ff2bc028346028cf005cc92b57
GUGOFO <gugofogomes@gmail.com>
2026-02-26 00:00:19 -0300

    Chat extraindo todos os dados do historico

---
+++ APIs/OlhoDeSauron/index.js
@@ -0,0 +1,95 @@
+const dropzone = document.getElementById("containerDoInput");
+const inputFile = document.getElementById("inputImage");
+const textoDropzone = dropzone.querySelector("h2"); 
+const btnAnalisar = document.getElementById("btnAnalisar");
+
+const geminiKey = ""; 
+const modelo = "gemini-2.5-flash";
+const url = `https://generativelanguage.googleapis.com/v1beta/models/${modelo}:generateContent?key=${geminiKey}`;
+
+
+dropzone.addEventListener("dragover", (evento) => {
+    evento.preventDefault();
+    dropzone.style.backgroundColor = "rgba(255, 255, 255, 0.4)"; 
+});
+
+dropzone.addEventListener("dragleave", () => {
+    dropzone.style.backgroundColor = "rgba(255, 255, 255, 0.2)"; 
+});
+
+dropzone.addEventListener("drop", (evento) => {
+    evento.preventDefault(); 
+    dropzone.style.backgroundColor = "rgba(255, 255, 255, 0.2)"; 
+
+    const arquivosArrastados = evento.dataTransfer.files;
+
+    if (arquivosArrastados.length > 0) {
+        inputFile.files = arquivosArrastados;
+        textoDropzone.textContent = inputFile.files[0].name;
+    }
+});
+
+inputFile.addEventListener("change", () => {
+    if (inputFile.files.length > 0) {
+        textoDropzone.textContent = inputFile.files[0].name;
+    }
+});
+
+async function arquivoParaBase64(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.readAsDataURL(arquivo);
+        leitor.onload = () => {
+            const base64 = leitor.result.split(',')[1]; 
+            resolve(base64);
+        };
+        leitor.onerror = (erro) => reject(erro);
+    });
+}
+
+btnAnalisar.addEventListener("click", async () => {
+    const arquivo = inputFile.files[0];
+
+    if(!arquivo){
+        console.log("Voce n enviou nada");
+        return;
+    }
+
+    btnAnalisar.disabled = true;
+    btnAnalisar.value = "Carregando...";
+    
+    try{
+        const base64DoPDF = await arquivoParaBase64(arquivo);
+
+        const payload = {
+            contents: [{
+                parts: [
+                    { text: ```Analise este histórico escolar.
+                               Extraia os dados das disciplinas cursadas.
+                               ``` },
+                    { inlineData: { mimeType: "application/pdf", data: base64DoPDF } }
+                ]
+            }]
+        };
+
+        const resposta = await fetch(url, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(payload)
+        });
+
+        if(!resposta.ok) throw new Error(`Erro requisição HTTP: ${resposta.status}`);
+
+        const dados = await resposta.json();
+        const respostaDaIA = dados.candidates[0].content.parts[0].text;
+
+        console.log(respostaDaIA);
+    }
+    catch(error){
+        console.error(error);
+    }
+    finally{
+        btnAnalisar.disabled = false;
+        btnAnalisar.value = "Enviar Historico"
+    }
+})
