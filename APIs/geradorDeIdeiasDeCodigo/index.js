f98fdd1ce59c9c1c21cd5d3f002f28f9ea4e3a9a
GUGOFO <gugofogomes@gmail.com>
2026-03-01 01:06:53 -0300

    adicionei memoria para ia

--- APIs/geradorDeIdeiasDeCodigo/index.js
+++ APIs/geradorDeIdeiasDeCodigo/index.js
@@ -1,8 +1,9 @@
 
 const form = document.getElementById("divGerador")
 const btn = document.getElementById("inputDoUsuario")
+let historicoDeConversas = [];
 
-const geminiKey = ""; 
+const geminiKey = "SUA CHAVE AQUIII"; 
 const modelo = "gemini-2.5-flash";
 const url = `https://generativelanguage.googleapis.com/v1beta/models/${modelo}:generateContent?key=${geminiKey}`;
 
@@ -11,6 +12,11 @@ form.addEventListener("submit", async e => {
         e.preventDefault()
         btn.disabled = true;
         btn.value = "carregando..."
+
+        historicoDeConversas.push({
+            role: "user",
+            parts: [{ text: "Me de uma nova ideia" }]
+        })
         
         const personalidade = `Voce é uma maquina feita para dar UMA IDEIA de novo projeto para 
                                um aluno de engenharia de software que esta 
@@ -18,18 +24,13 @@ form.addEventListener("submit", async e => {
                                ele esta em um nivel iniciante / moderado
                                 
                                Não é necessário ter detalhes, quero que voce retorne o titulo da ideia seguido por POUCAS especificações de dsine (NO MAXIMO UM PARAGRAFO SEM ENROLAÇÃO)`
-    
-        console.log("odido")
         
         const resposta = await fetch(url, {
             method: "POST",
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 systemInstruction: { parts: [{ text: personalidade }]},
-                "contents": [{
-                    "parts": [{
-                        "text": "Me de uma nova ideia"
-                    }]}]
+                contents: historicoDeConversas
             })
         });
         if(!resposta.ok) throw new Error("deu ruim quando postou")
@@ -41,11 +42,16 @@ form.addEventListener("submit", async e => {
         mostrarMsg("respostaDaIa", respostaDaIA)
     
         console.log(respostaDaIA)
+
+        historicoDeConversas.push({
+            role: "model",
+            parts: [{ text: respostaDaIA }]
+        })
     }
     catch(error){
         console.error(error)
-        console.log("oi")
         mostrarMsg("error", "DEU MERDA MAN \nOLHA O CONSOLE AGORA")
+        historicoDeConversas.pop();
     }
     finally{
         btn.disabled = false;
