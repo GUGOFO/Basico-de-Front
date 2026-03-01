d64020a2d7df3cd4975015995ef8750deee0b987
GUGOFO <gugofogomes@gmail.com>
2026-03-01 00:06:32 -0300

    Avanços no css

--- APIs/geradorDeIdeiasDeCodigo/index.js
+++ APIs/geradorDeIdeiasDeCodigo/index.js
@@ -1,4 +1,4 @@
 
-const geminiKey = "msdmas"; 
+const geminiKey = "AIzaSyDcalIkfXOgyvrO9vE46yRQLxtaCw8HdAs"; 
 const modelo = "gemini-3-flash-preview";
 const url = `https://generativelanguage.googleapis.com/v1beta/models/${modelo}:generateContent?key=${geminiKey}`;
